395a9cc250dc3b7a17704bf4824f5a9687b47d7d
Areesha-sites <areesha21314@gmail.com>
2024-12-07 20:12:01 +0500

    updated

--- src/app/Components/Header.tsx
+++ src/app/Components/Header.tsx
@@ -7,7 +7,7 @@ const TopHeader = () => {
         <div className="w-[859px] h-[24px] absolute  right-[136px] top-[14px] flex flex-row items-center gap-[231px]">
           <div className="left w-[550px] h-[24px] gap-[8px] flex ">
             <p className="w-[474px] h-[18px] whitespace-nowrap font-poppins text-white text-[14px] leading-[21px]">
-              Summer Sale For All Swim Suits And Free Express Delivery - OFF
+              Summer Sale For All hey Swim Suits And Free Express Delivery - OFF
               50%!
             </p>
             <p className="font-semibold text-[14px] w-[68px] h-[24px] font-poppins text-whiteText underline">
