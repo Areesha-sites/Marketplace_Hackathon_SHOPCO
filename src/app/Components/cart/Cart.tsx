d63410e950e8f1474d1a7334665dad07d09cf954
Areesha-sites <areesha21314@gmail.com>
2024-12-16 18:12:12 +0500

    updated

--- src/app/Components/cart/Cart.tsx
+++ src/app/Components/cart/Cart.tsx
@@ -11,7 +11,7 @@ const Cart = () => {
           data-aos-delay="200"
           className="md:w-[106px] w-[89px] h-[22px] absolute md:top-[158px] md:left-[100px] left-[16px] flex gap-[12px] items-center top-[118px]"
         >
-          <span className="font-inter text-[16px] font-normal text-black/60">
+          <span className="font-satoshi text-[16px] font-normal text-black/60">
             Home
           </span>
           <Image
@@ -21,14 +21,14 @@ const Cart = () => {
             width={16}
             className="h-[16px] w-[16px]"
           />
-          <span className="font-inter text-[16px] font-normal text-black">
+          <span className="font-satoshi text-[16px] font-normal text-black">
             Cart
           </span>
         </div>
         <h1
           data-aos="zoom-in"
           data-aos-delay="300"
-          className="absolute md:top-[204px] left-[16px] md:left-[100px] text-black font-black uppercase md:text-[40px] text-[32px] top-[145px]"
+          className="absolute md:top-[204px] left-[16px] md:left-[100px] text-black font-black uppercase md:text-[40px] text-[32px] top-[145px] font-integralCf"
         >
           Your cart
         </h1>
@@ -51,28 +51,28 @@ const Cart = () => {
             <div className="md:w-[527px] md:h-[124px] flex justify-between">
               <div className="md:w-[227px] md:h-[118px] flex justify-between ">
                 <div className="md:w-[227px] md:h-[71px] w-[217px] h-[57px] flex flex-col gap-[2px] relative left-[100px] md:static">
-                  <h1 className="md:text-[20px] text-[16px] md:font-bold font-medium text-black font-satoshi whitespace-nowrap ">
+                  <h1 className="md:text-[20px] text-[16px] md:font-bold font-medium text-black font-satoshiBold whitespace-nowrap ">
                     Gradient Graphic T-shirt
                   </h1>
                   <div className="w-[78px] h-[42px] flex flex-col gap-[4px]">
                     <p className="md:text-[14px] text-[12px] font-satoshi text-black font-normal ">
                       Size:{" "}
-                      <span className="text-black opacity-50 font-inter">
+                      <span className="text-black opacity-50 font-satoshi">
                         {" "}
                         Large
                       </span>
                     </p>
                   </div>
                   <div className="w-[78px] h-[42px] flex flex-col gap-[4px]">
-                    <p className="md:text-[14px] text-[12px] font-inter text-black font-normal whitespace-nowrap">
+                    <p className="md:text-[14px] text-[12px] font-satoshi text-black font-normal whitespace-nowrap">
                       Color:{" "}
-                      <span className="text-black opacity-50 font-inter">
+                      <span className="text-black opacity-50 font-satoshi">
                         {" "}
                         White
                       </span>
                     </p>
                   </div>
-                  <h2 className="md:text-[24px] text-[20px] font-inter font-bold text-black mt-2">
+                  <h2 className="md:text-[24px] text-[20px] font-satoshi font-bold text-black mt-2">
                     $145
                   </h2>
                 </div>
@@ -119,24 +119,24 @@ const Cart = () => {
             <div className="md:w-[527px] md:h-[124px] w-[330px] h-[99px] flex justify-between">
               <div className="md:w-[227px] md:h-[118px] flex justify-between ">
                 <div className="md:w-[227px] md:h-[71px] flex flex-col gap-[2px] relative left-[100px] md:static">
-                  <h1 className="md:text-[20px] text-[14px] md:font-bold font-medium text-black whitespace-nowrap font-inter">
+                  <h1 className="md:text-[20px] text-[14px] md:font-bold font-medium text-black whitespace-nowrap font-satoshiBold">
                     CHECKERED SHIRT
                   </h1>
                   <div className="w-[78px] h-[42px] flex flex-col gap-[4px]">
-                    <p className="md:text-[14px] text-[12px] font-inter text-black font-normal whitespace-nowrap">
+                    <p className="md:text-[14px] text-[12px] font-satoshi text-black font-normal whitespace-nowrap">
                       Size:{" "}
-                      <span className="text-black opacity-50 font-inter">
+                      <span className="text-black opacity-50 font-satoshi">
                         {" "}
                         Medium
                       </span>
                     </p>
                   </div>
                   <div className="w-[78px] h-[42px] flex flex-col gap-[4px]">
-                    <p className="md:text-[14px] text-[12px] font-inter text-black font-normal whitespace-nowrap">
+                    <p className="md:text-[14px] text-[12px] font-satoshi text-black font-normal whitespace-nowrap">
                       Color: <span className="text-black opacity-50"> red</span>
                     </p>
                   </div>
-                  <h2 className="md:text-[24px] text-[20px] font-inter font-bold text-black mt-2">
+                  <h2 className="md:text-[24px] text-[20px] font-satoshi font-bold text-black mt-2">
                     $180
                   </h2>
                 </div>
@@ -158,7 +158,7 @@ const Cart = () => {
                     width={20}
                     className="md:h-[20px] md:w-[20px] h-[16px] w-[16px] "
                   />
-                  <span className="text-[14px] text-black font-bold">1</span>
+                  <span className="text-[14px] text-black font-bold font-inter">1</span>
                   <Image
                     src="/increase.svg"
                     alt="increase"
@@ -170,7 +170,7 @@ const Cart = () => {
               </div>
             </div>
           </div>
-          <div className="md:w-[667px] w-[330px]  border-b-[1px] border-black/10"></div>
+          <div className="md:w-[667px] w-[330px] border-b-[1px] border-black/10"></div>
           <div className="md:w-[667px]  md:h-[124px] flex gap-[16px] ">
             <div className="md:w-[124px] md:h-[124px] h-[99px] w-[99px] rounded-[8.66px] bg-bgLightGrayColor">
               <Image
@@ -184,28 +184,28 @@ const Cart = () => {
             <div className="md:w-[527px] md:h-[124px] flex justify-between">
               <div className="md:w-[227px] md:h-[118px] flex justify-between ">
                 <div className="md:w-[227px] md:h-[71px] flex flex-col gap-[2px] relative left-[100px]  md:left-[160px] md:absolute">
-                  <h1 className="md:text-[20px] md:font-bold font-medium text-[14px] text-blackfont-inter  whitespace-nowrap">
+                  <h1 className="md:text-[20px] md:font-bold font-medium text-[14px] text-black font-satoshiBold   whitespace-nowrap">
                     SKINNY FIT JEANS
                   </h1>
                   <div className="md:w-[78px] md:h-[42px] flex flex-col gap-[4px]">
-                    <p className="md:text-[14px] text-[12px] font-inter text-black font-normal ">
+                    <p className="md:text-[14px] text-[12px] font-satoshi text-black font-normal ">
                       Size:{" "}
-                      <span className="text-black opacity-50 font-inter">
+                      <span className="text-black opacity-50 font-satoshi">
                         {" "}
                         Large
                       </span>
                     </p>
                   </div>
                   <div className="md:w-[78px] md:h-[42px] flex flex-col gap-[4px]">
-                    <p className="md:text-[14px] text-[12px] font-inter text-black font-normal whitespace-nowrap">
+                    <p className="md:text-[14px] text-[12px] font-satoshi text-black font-normal whitespace-nowrap">
                       Color:{" "}
-                      <span className="text-black opacity-50 font-inter">
+                      <span className="text-black opacity-50 font-satoshi">
                         {" "}
                         blue
                       </span>
                     </p>
                   </div>
-                  <h2 className="md:text-[24px] text-[20px] font-inter font-bold text-black mt-2">
+                  <h2 className="md:text-[24px] text-[20px] font-satoshi font-bold text-black mt-2">
                     $240
                   </h2>
                 </div>
@@ -227,7 +227,7 @@ const Cart = () => {
                     width={20}
                     className="md:h-[20px] md:w-[20px] h-[16px] w-[16px] "
                   />
-                  <span className="text-[14px] text-black font-inter font-bold">
+                  <span className="text-[14px] text-black font-satoshi font-bold">
                     1
                   </span>
                   <Image
@@ -248,40 +248,40 @@ const Cart = () => {
           data-aos-duration="2000"
           className="md:w-[505px] md:h-[458px] absolute md:top-[276px] md:left-[835px] md:py-[20px] md:px-[24px] rounded-[20px] border-[1px] border-black/10 flex flex-col md:gap-[24px] w-[358px] h-[390px] top-[612px] left-[16px] p-[20px] gap-[16px]"
         >
-          <h2 className="font-inter md:text-[24px] text-[20px] font-bold text-black">
+          <h2 className="font-satoshiBold md:text-[24px] text-[20px] font-bold text-black">
             Order Summary
           </h2>
           <div className="md:w-[457px] md:h-[193px] flex flex-col gap-[20px] w-[313px] h-[173px]  ">
             <div className="md:w-[457px] md:h-[27px] flex justify-between">
-              <p className="md:text-[20px]  text-[16px] font-normal font-inter text-black/60">
+              <p className="md:text-[20px]  text-[16px] font-normal font-satoshi text-black/60">
                 Subtotal
               </p>
-              <p className="md:text-[20px] text-[16px] font-bold font-inter text-black">
+              <p className="md:text-[20px] text-[16px] font-bold font-satoshi text-black">
                 $565
               </p>
             </div>
             <div className="md:w-[457px] md:h-[27px] flex justify-between">
-              <p className="md:text-[20px]  text-[16px] font-normal font-inter text-black/60">
+              <p className="md:text-[20px]  text-[16px] font-normal font-satoshi text-black/60">
                 Discount (-20%)
               </p>
-              <p className="md:text-[20px] font-bold text-[16px] font-inter text-redTextOfferColor">
+              <p className="md:text-[20px] font-bold text-[16px] font-satoshi text-redTextOfferColor">
                 -$113
               </p>
             </div>
             <div className="md:w-[457px] md:h-[27px] flex justify-between">
-              <p className="md:text-[20px] text-[16px] font-normal font-inter text-black/60">
+              <p className="md:text-[20px] text-[16px] font-normal font-satoshi text-black/60">
                 Delivery Fee
               </p>
-              <p className="md:text-[20px] font-bold font-inter text-black">
+              <p className="md:text-[20px] font-bold font-satoshi text-black">
                 $15
               </p>
             </div>
             <div className="md:w-[457px] border-b-[1px] border-black/10"></div>
             <div className="md:w-[457px] md:h-[27px] flex justify-between">
-              <p className="md:text-[20px] font-normal font-inter text-black">
+              <p className="md:text-[20px] font-normal font-satoshiBold text-black">
                 Total
               </p>
-              <p className="md:text-[24px] font-bold  text-[20px] font-inter text-black">
+              <p className="md:text-[24px] font-bold  text-[20px] font-satoshi text-black">
                 $467
               </p>
             </div>
@@ -295,16 +295,16 @@ const Cart = () => {
                 width={24}
                 className="md:h-[24px] md:w-[24px] h-[20px] w-[20px]"
               />
-              <p className="md:text-[16px] text-[14px] font-inter font-normal text-black/40">
+              <p className="md:text-[16px] text-[14px] font-satoshi font-normal text-black/40">
                 Add promo code
               </p>
             </div>
-            <button className="py-[12px] px-[16px] bg-black text-white rounded-[62px] font-inter md:text-[16px] text-[14px] font-medium md:w-[119px] h-[48px] w-[88px]">
+            <button className="py-[12px] px-[16px] bg-black text-white rounded-[62px] font-satoshi md:text-[16px] text-[14px] font-medium md:w-[119px] h-[48px] w-[88px]">
               Apply
             </button>
           </div>
           <div className="md:w-[457px] md:h-[60px] md:py-[16px] md:px-[56px] px-[54px] py-[14px] rounded-[62px] bg-black text-white flex items-center gap-[12px] justify-center">
-            <span className="md:text-[16px] text-[14px] font-inter font-medium">
+            <span className="md:text-[16px] text-[14px] font-satoshi font-medium">
               Go to Checkout
             </span>
             <Image
